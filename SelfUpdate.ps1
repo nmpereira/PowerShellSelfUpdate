70b1e42208cfc67496fedcc745be480bf73ac156
nmpereira <45009203+nmpereira@users.noreply.github.com>
2021-03-18 01:53:44 -0400

    1

---
+++ SelfUpdate.ps1
@@ -0,0 +1,43 @@
+function DownloadFilesFromRepo {
+#Param(
+    $Owner= 'nmpereira'
+    $Repository='PowerShellTest'
+    $Path= 'Test1.ps1'
+    $DestinationPath= 'C:\Gitpersonal\testcode'
+    #)
+
+    $baseUri = "https://api.github.com/"
+    $args = "repos/$Owner/$Repository/contents/$Path"
+    $wr = Invoke-WebRequest -Uri $($baseuri+$args)
+    $objects = $wr.Content | ConvertFrom-Json
+    $files = $objects | where {$_.type -eq "file"} | Select -exp download_url
+    $directories = $objects | where {$_.type -eq "dir"}
+    
+    $directories | ForEach-Object { 
+        DownloadFilesFromRepo -Owner $Owner -Repository $Repository -Path $_.path -DestinationPath $($DestinationPath+$_.name)
+    }
+
+    
+    if (-not (Test-Path $DestinationPath)) {
+        # Destination path does not exist, let's create it
+        try {
+            New-Item -Path $DestinationPath -ItemType Directory -ErrorAction Stop
+        } catch {
+            throw "Could not create path '$DestinationPath'!"
+        }
+    }
+
+    foreach ($file in $files) {
+        $fileDestination = Join-Path $DestinationPath (Split-Path $file -Leaf)
+        try {
+            Invoke-WebRequest -Uri $file -OutFile $fileDestination -ErrorAction Stop -Verbose
+            "Grabbed '$($file)' to '$fileDestination'"
+        } catch {
+            throw "Unable to download '$($file.path)'"
+        }
+    }
+
+}
+
+(DownloadFilesFromRepo)
+pause
